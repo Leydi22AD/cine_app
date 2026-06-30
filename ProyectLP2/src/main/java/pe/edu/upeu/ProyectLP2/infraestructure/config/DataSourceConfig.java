9b1d38c3da9ff8b8f430fc55a2c5259349eef482
Leydi22AD <leydi.arevalo@upeu.edu.pe>
2026-06-30 04:50:30 -0500

    test.yml v2

--- ProyectLP2/src/main/java/pe/edu/upeu/ProyectLP2/infraestructure/config/DataSourceConfig.java
+++ ProyectLP2/src/main/java/pe/edu/upeu/ProyectLP2/infraestructure/config/DataSourceConfig.java
@@ -3,10 +3,12 @@ package pe.edu.upeu.ProyectLP2.infraestructure.config;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
+@Profile("!test")
 public class DataSourceConfig {
 
     @Bean

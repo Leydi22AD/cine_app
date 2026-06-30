3a46821e9866e579e0dbf36c7400305857f0c042
Leydi22AD <leydi.arevalo@upeu.edu.pe>
2026-06-30 07:05:58 -0500

    actualizado ProyectLp2ApplicationTests.java con la anotación @ActiveProfiles("test").

--- ProyectLP2/src/test/java/pe/edu/upeu/ProyectLP2/CucumberSpringConfiguration.java
+++ ProyectLP2/src/test/java/pe/edu/upeu/ProyectLP2/CucumberSpringConfiguration.java
@@ -3,11 +3,13 @@ package pe.edu.upeu.ProyectLP2;
 import io.cucumber.java.Before; // ⚠️ Asegúrate de que sea el Before de Cucumber, NO de JUnit
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestPropertySource(locations = "classpath:application-test.yml")
+@ActiveProfiles("test")
 public class CucumberSpringConfiguration {
 
     @Before

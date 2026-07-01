1772b5660fb0407ed955447841e4c4a853a1866f
Leydi22AD <leydi.arevalo@upeu.edu.pe>
2026-07-01 10:20:42 -0500

    UsuarioMapper para que ignore la propiedad correo que estaba causando el conflicto.

--- ProyectLP2/src/main/java/pe/edu/upeu/ProyectLP2/infraestructure/adapter/mapper/UsuarioMapper.java
+++ ProyectLP2/src/main/java/pe/edu/upeu/ProyectLP2/infraestructure/adapter/mapper/UsuarioMapper.java
@@ -1,7 +1,8 @@
 package pe.edu.upeu.ProyectLP2.infraestructure.adapter.mapper;
 
-
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import pe.edu.upeu.ProyectLP2.domain.model.Usuario;
 import pe.edu.upeu.ProyectLP2.infraestructure.entity.UsuarioEntity;
 
@@ -10,13 +11,12 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
-    // Convierte de Entity a Domain
+    @Mapping(target = "correo", ignore = true)
     Usuario toDomainModel(UsuarioEntity entity);
-
-    // Convierte de Domain a Entity
-    UsuarioEntity toEntity(Usuario usuario);
 
     List<Usuario> toDomainModelList(List<UsuarioEntity> entities);
 
-
+    @InheritInverseConfiguration
+    @Mapping(target = "tickets", ignore = true)
+    UsuarioEntity toEntity(Usuario domainModel);
 }

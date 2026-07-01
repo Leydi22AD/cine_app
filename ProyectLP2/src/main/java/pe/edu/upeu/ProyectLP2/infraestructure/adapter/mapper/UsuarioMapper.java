59bd13c7b667dd8676a545a808e81c06f45faacf
Leydi22AD <leydi.arevalo@upeu.edu.pe>
2026-07-01 15:54:23 -0500

    prueba final

--- ProyectLP2/src/main/java/pe/edu/upeu/ProyectLP2/infraestructure/adapter/mapper/UsuarioMapper.java
+++ ProyectLP2/src/main/java/pe/edu/upeu/ProyectLP2/infraestructure/adapter/mapper/UsuarioMapper.java
@@ -1,7 +1,6 @@
 package pe.edu.upeu.ProyectLP2.infraestructure.adapter.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import pe.edu.upeu.ProyectLP2.domain.model.Usuario;
 import pe.edu.upeu.ProyectLP2.infraestructure.entity.UsuarioEntity;
 
@@ -10,12 +9,9 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
-    @Mapping(source = "email", target = "correo")
     Usuario toDomainModel(UsuarioEntity entity);
 
     List<Usuario> toDomainModelList(List<UsuarioEntity> entities);
 
-    @Mapping(source = "correo", target = "email")
-    // La anotación @Mapping para 'tickets' ha sido eliminada porque era incorrecta.
     UsuarioEntity toEntity(Usuario domainModel);
 }

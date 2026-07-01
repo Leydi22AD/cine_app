c94eba195e0feec143de748a0b4558a13fd3523c
Leydi22AD <leydi.arevalo@upeu.edu.pe>
2026-07-01 10:32:31 -0500

    UsuarioMapper eliminando las configuraciones erróneas. Ahora MapStruct debería poder manejar el mapeo de email

--- ProyectLP2/src/main/java/pe/edu/upeu/ProyectLP2/infraestructure/adapter/mapper/UsuarioMapper.java
+++ ProyectLP2/src/main/java/pe/edu/upeu/ProyectLP2/infraestructure/adapter/mapper/UsuarioMapper.java
@@ -1,6 +1,5 @@
 package pe.edu.upeu.ProyectLP2.infraestructure.adapter.mapper;
 
-import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import pe.edu.upeu.ProyectLP2.domain.model.Usuario;
@@ -11,12 +10,20 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
-    @Mapping(target = "correo", ignore = true)
+    /**
+     * Mapea una entidad UsuarioEntity a un modelo de dominio Usuario.
+     */
     Usuario toDomainModel(UsuarioEntity entity);
 
+    /**
+     * Mapea una lista de entidades UsuarioEntity a una lista de modelos de dominio Usuario.
+     */
     List<Usuario> toDomainModelList(List<UsuarioEntity> entities);
 
-    @InheritInverseConfiguration
+    /**
+     * Mapea un modelo de dominio Usuario a una entidad UsuarioEntity.
+     * Se ignora la propiedad 'tickets' para evitar ciclos de mapeo en relaciones bidireccionales.
+     */
     @Mapping(target = "tickets", ignore = true)
     UsuarioEntity toEntity(Usuario domainModel);
 }
